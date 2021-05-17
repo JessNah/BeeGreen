2ac5a3b820731feb024a93190db96e220b44093b
Jessica Nahulan <jess.n169@hotmail.com>
2021-05-17 09:52:05 -0400

    convert root contentScript to state component

--- Extension/src/contentScript/contentScript.tsx
+++ Extension/src/contentScript/contentScript.tsx
@@ -1,57 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import { getStoredOptions, LocalStorageOptions } from '../utils/storage'
 import { Messages } from '../utils/messages'
 import './contentScript.css'
 
-const App: React.FC<{}> = () => {
-  const [options, setOptions] = useState<LocalStorageOptions | null>(null)
-  const [isActive, setIsActive] = useState<boolean>(false)
-  const [isEnabled, setIsEnabled] = useState<boolean>(false)
-  const [showItemDialog, setshowItemDialog] = useState<boolean>(false)
-  const [isCheckout, setIsCheckout] = useState<boolean>(true)
+interface AppProps {
+  //TODO
+}
 
-  useEffect(() => {
+interface AppState {
+  options: {[key:string]: any} | null,
+  isCheckout: boolean,
+  isEnabled: boolean,
+  isActive: boolean,
+  showItemDialog: boolean
+}
+
+class App extends Component<AppProps, AppState> {
+  state = {
+    options: null,
+    isCheckout: true,
+    isEnabled: false,
+    isActive: false,
+    showItemDialog: false
+  }
+
+  componentDidMount() {
+    chrome.runtime.onMessage.addListener(this.handleMessages)
     getStoredOptions().then((options) => {
       console.log("contentScript received options:")
       console.log(options);
-      setOptions(options)
-      setIsEnabled(options.hasAutoOverlay)
+      this.setState({options: options, isEnabled: options.hasAutoOverlay});
     })
-  }, [])
+  }
 
-  const handleMessages = (msg: Messages) => {
+  componentWillUnmount() {
+     // clean up event listener
+     chrome.runtime.onMessage.removeListener(this.handleMessages)
+  }
+
+  handleMessages = (msg: Messages) => {
     console.log("received message: " + msg);
     if (msg === Messages.ENABLE_OVERLAY) {
-      isEnabled && !isActive && setIsActive(true)
+      this.setState(prevState => {
+        if(prevState.isEnabled && !prevState.isActive){
+          return {isActive: true}
+        } else { return null}})
     } else if (msg === Messages.DISABLE_OVERLAY) {
-      isActive && setIsActive(false)
+      this.setState(prevState => {
+        if(prevState.isActive){
+          return {isActive: false}
+        } else { return null}})
     } else if (msg === Messages.SET_IS_CHECKOUT) {
-      !isCheckout && setIsCheckout(true)
+      this.setState(prevState => {
+        if(!prevState.isCheckout){
+          return {isCheckout: true}
+        } else { return null}})
     } else if (msg === Messages.SET_IS_NOT_CHECKOUT) {
-      isCheckout && setIsCheckout(false)
+      this.setState(prevState => {
+        if(prevState.isCheckout){
+          return {isCheckout: false}
+        } else { return null}})
     }
   }
 
-  useEffect(() => {
-    chrome.runtime.onMessage.addListener(handleMessages)
-    return () => {
-      // clean up event listener
-      chrome.runtime.onMessage.removeListener(handleMessages)
+  render() {
+    if (!this.state.options) {
+      return null
     }
-  })
-
-  if (!options) {
-    return null
+    this.state.isActive && console.log("contentScript is in the active state");
+    return (
+      <>
+        {this.state.isActive && (
+          <div>Active dom element</div>
+        )}
+      </>
+    )
   }
-  isActive && console.log("render tag/dialog")
-  return (
-    <>
-      {isActive && (
-        <div>Active dom element</div>
-      )}
-    </>
-  )
 }
 
 const root = document.createElement('div')
