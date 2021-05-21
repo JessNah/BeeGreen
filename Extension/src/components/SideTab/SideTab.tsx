f01df4e33a39d55848982b177cbda2867d9ee0aa
Jessica Nahulan <jess.n169@hotmail.com>
2021-05-20 22:37:07 -0400

    add sidetab close functionality

--- Extension/src/components/SideTab/SideTab.tsx
+++ Extension/src/components/SideTab/SideTab.tsx
@@ -2,11 +2,18 @@ import React, { useEffect, useState } from 'react'
 import "./SideTab.css"
 
 const SideTab: React.FC<{}> = () => {
+  const [closeTab, setCloseTab] = useState<boolean>(false)
+  if(closeTab){
+    return null;
+  }
   return (
     <>
       <div className="take2-side-tab">
-        <div className={"take2-side-tab-icon take2-side-tab-default"} aria-hidden="true">T2</div>
-        <div className={"take2-side-tab-icon take2-side-tab-close"}>
+        <div className={"take2-side-tab-icon take2-side-tab-default"} aria-hidden="true">
+          T2
+        </div>
+        <div className={"take2-side-tab-icon take2-side-tab-close"}
+          onClick={() => setCloseTab(true)}>
           <svg focusable="false" preserveAspectRatio="xMidYMid meet" aria-label="Close this tab" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 32 32" role="img" className="take2-close-svg" ><path d="M24 9.4L22.6 8 16 14.6 9.4 8 8 9.4l6.6 6.6L8 22.6 9.4 24l6.6-6.6 6.6 6.6 1.4-1.4-6.6-6.6L24 9.4z"></path></svg>
         </div>
       </div>
