1d73f0167d5c1faf62fee4454fb2d3ce0a11b055
Jessica Nahulan <jess.n169@hotmail.com>
2021-05-21 23:29:08 -0400

    support tags and multi-store cart in instacart

--- Extension/src/components/CheckoutDialog/CheckoutDialog.tsx
+++ Extension/src/components/CheckoutDialog/CheckoutDialog.tsx
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
 import "./CheckoutDialog.scss"
 import { Tearsheet } from "@carbon/ibm-cloud-cognitive"
-import { getInstaCart } from "../../stores/getInstaCart" 
-import { Accordion, AccordionItem } from "carbon-components-react"
+import { getInstaCart, getCartAverageRating } from "../../stores/getInstaCart" 
+import { Accordion, AccordionItem, Tag, TagTypeName } from "carbon-components-react"
 
 interface CheckoutDialogProps {
   closeCheckoutDialog: () => void;
@@ -21,17 +21,72 @@ class CheckoutDialog extends Component<CheckoutDialogProps, CheckoutDialogState>
     this.setState({open: true});
   }
 
+  getStyledTagScore = (score) => {
+    let style = "red";
+    if(score >= 8){
+      style = "green";
+    } else if (score >= 6){
+      style = "teal";
+    } else if (score >= 4) {
+      style = "purple";
+    } else if (score >= 2) {
+      style = "magenta";
+    } else {
+      style = "red";
+    }
+    return style as TagTypeName;
+  }
+
+  getScoreText = (score) => {
+    let text = "Excellent";
+    if(score >= 8){
+      text = "Excellent";
+    } else if (score >= 6){
+      text = "Very good";
+    } else if (score >= 4) {
+      text = "Good";
+    } else if (score >= 2) {
+      text = "Fair";
+    } else {
+      text = "Low";
+    }
+    return text;
+  }
+
   render() {
     const cart = getInstaCart();
+    const cartRank = getCartAverageRating(cart);
     console.log(cart);
     return (
       <>
         <Tearsheet
+          open={this.state.open}
           label={""}
           title={"Take2"}
           description={"Keep it up! You're shopping smarter than 73% of shoppers visiting this site."}
+          actions={[{
+            kind: 'secondary',
+            label: "Back",
+            loading: false,
+            onClick: () => {this.props.closeCheckoutDialog()}
+            }, {
+            kind: 'ghost',
+            label: "Make an impact! Donate to plant some trees and reduce the carbon footprint of your purchase!",
+            loading: false,
+            onClick: () => {window.open("https://carbonfund.org/product-category/plant-trees/")}
+          }]}
+          influencerPosition={'right'}
+          influencerWidth={'wide'}
           influencer={<>
-            <div>Insert shopping cart</div>
+            <div className={"take2--checkout-dialog-card-heading-wrapper"}>
+              <div className={"take2--checkout-dialog-cart-heading"}>
+                Shopping cart rating
+              </div>
+              <Tag
+                type={this.getStyledTagScore(cartRank)}>
+                {this.getScoreText(cartRank)}
+              </Tag>
+            </div>
             <Accordion>
             {cart.map((item, index) => {
               return (
@@ -49,7 +104,14 @@ class CheckoutDialog extends Component<CheckoutDialogProps, CheckoutDialogState>
                         </div>
                       </div>
                       <div className={"take2--checkout-dialog-cart-item-text-container"}>
-                        <span className={"take2--checkout-dialog-cart-item-score"}>{"3000"}</span>
+                        <div className={"take2--checkout-dialog-cart-item-text-wrapper"}>
+                          <span className={"take2--checkout-dialog-cart-item-score"}>
+                              <Tag
+                               type={this.getStyledTagScore(item.score)} >
+                                {this.getScoreText(item.score)}
+                              </Tag>
+                          </span>
+                        </div>
                       </div>
                     </div>
                   }
@@ -58,16 +120,7 @@ class CheckoutDialog extends Component<CheckoutDialogProps, CheckoutDialogState>
             }
             </Accordion>
             </>}
-          influencerPosition={'right'}
-          influencerWidth={'wide'}
-          actions={[{
-            kind: 'secondary',
-            label: "Back",
-            loading: false,
-            onClick: () => {this.props.closeCheckoutDialog()}
-          }]
-          }
-          open={this.state.open}>
+            >
         </Tearsheet>
       </>
     )
--- Extension/src/stores/getInstaCart.js
+++ Extension/src/stores/getInstaCart.js
@@ -8,7 +8,7 @@ export function getInstaCart() {
     let imageIndex = 0;
     for(let i = 0; i < _cartItems.length; i++){
         const cartItem = _cartItems[i];
-        if(_cartImages[i].parentElement.className === "RetailerLogo"){
+        if(_cartImages[imageIndex].parentElement.className === "RetailerLogo"){
             imageIndex++;
         }
         let _item = {
@@ -19,5 +19,36 @@ export function getInstaCart() {
         cart.push(_item);
         imageIndex++;
     }
+    cart = analyzeCart(cart);
     return cart;
+}
+
+export function analyzeCart(cart) {
+    for(let item of cart){
+        item.score = Math.floor(Math.random() * 10) + 1;
+        if(item.score > 10){
+            item.score = 10;
+        }
+    }
+    cart.sort(compare);
+    return cart;
+}
+
+function compare( a, b ) {
+    if ( a.score < b.score ){
+      return 1;
+    }
+    if ( a.score > b.score ){
+      return -1;
+    }
+    return 0;
+}
+
+export function getCartAverageRating(cart) {
+    let score = 0;
+    for(let item of cart){
+        score += item.score;
+    }
+    console.log(score);
+    return score/cart.length;
 }
