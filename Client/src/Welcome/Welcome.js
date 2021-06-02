9c016a2ff68c277be4ab0da55498aebe038660b7
Jessica Nahulan <jess.n169@hotmail.com>
2021-06-02 17:10:29 -0400

    include bee image

--- Client/src/Welcome/Welcome.js
+++ Client/src/Welcome/Welcome.js
@@ -3,6 +3,7 @@ import './Welcome.scss';
 import {ReactComponent as ReactSvg} from '../assets/undraw_add_to_cart_vkjp.svg';
 import withAnimationEaseIn from "../Utilities/withAnimationEaseIn/withAnimationEaseIn.js";
 import withAnimationEaseOut from "../Utilities/withAnimationEaseOut/withAnimationEaseOut.js";
+import { Bee32 } from "@carbon/icons-react"
 
 class Welcome extends Component {
 
@@ -12,7 +13,14 @@ class Welcome extends Component {
 			<React.Fragment>
 				<div className={"HeaderGradient"}>
 					<div className={"ApplicationName"}>
-						Welcome to Bee Green.
+						Welcome to Bee Green
+						<Bee32
+							style={{
+								marginLeft: "8px",
+								position: "absolute",
+								marginTop: "3px"
+							}}
+						/>
 					</div>
 					If we all make a small change, we can make a big impact.
 				</div>
