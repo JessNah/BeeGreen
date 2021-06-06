30a627c2bbd54e84c71f9d95c0be5dfba00961de
Jessica Nahulan <jess.n169@hotmail.com>
2021-06-06 11:29:50 -0400

    complete homepage

--- Client/src/Messages/messages_en.js
+++ Client/src/Messages/messages_en.js
@@ -7,5 +7,10 @@ export const messages_en = {
     contributeToDbHeaderBtn: "Contribute to our public database",
     profileHeaderBtn: "Profile",
     communityTextHeader: "On Bee Green, you'll find a like-minded community trying to make a difference!",
-    
+    welcomeBanner1: "We bring the information you need when shopping, right where it matters - in your cart",
+    welcomeBanner1Sub: "You don't need to feel alone, we're here to help.",
+    welcomeBanner1Btn: "Add to Chrome (100% Free)",
+    welcomeBanner2: "Automatic cart evaluations for carbon scoring and substitution recommendations!",
+    welcomeBanner2Sub: "An extension that does the research so you don't have to.",
+    welcomeBanner3: "How it works"
 }
--- Client/src/Welcome/Welcome.js
+++ Client/src/Welcome/Welcome.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import './Welcome.scss';
 import {ReactComponent as ReactSvg} from '../assets/undraw_add_to_cart_vkjp.svg';
+import {ReactComponent as ReactSvg2} from '../assets/undraw_empty_cart_co35.svg';
+import {ReactComponent as ReactSvg3} from '../assets/undraw_Online_information_re_erks.svg';
 import withAnimationEaseIn from "../Utilities/withAnimationEaseIn/withAnimationEaseIn.js";
 import withAnimationEaseOut from "../Utilities/withAnimationEaseOut/withAnimationEaseOut.js";
 import { Bee32 } from "@carbon/icons-react"
 import { messages_en } from "../Messages/messages_en";
+import { Button } from "carbon-components-react";
 
 class Welcome extends Component {
 
 	render(){
 		const Img = withAnimationEaseIn(ReactSvg);
+		const Img2 = withAnimationEaseIn(ReactSvg2);
+		const Img3 = withAnimationEaseIn(ReactSvg3);
 		return (
 			<React.Fragment>
 				<div className={"HeaderGradient"}>
@@ -32,6 +37,55 @@ class Welcome extends Component {
 				</div>
 				<div className={"CommunityWrapper"}>
 					{messages_en.communityTextHeader}
+				</div>
+				<div className={"BannerWrapperWelcome BannerWrapperFirst"}>
+					<div className={"BannerLeft"}>
+						<Img2 className={"Banner1Img"}/>
+					</div>
+					<div className={"BannerRight"}>
+						<div className={"BannerRight1Header"}>
+							{messages_en.welcomeBanner1}
+						</div>
+						<div className={"BannerRight1SubText"}>
+							{messages_en.welcomeBanner1Sub}
+						</div>
+						<Button 
+							className={"BannerButton"}
+							onClick={() => {console.log("TODO")}}>
+								{messages_en.welcomeBanner1Btn}
+						</Button>
+					</div>
+				</div>
+				<div className={"BannerWrapperWelcome2"}>
+					<div className={"BannerLeftText"} style={{paddingTop: "40px"}}>
+						<div className={"BannerRight1Header"}>
+							{messages_en.welcomeBanner2}
+						</div>
+						<div className={"BannerRight1SubText"}>
+							{messages_en.welcomeBanner2Sub}
+						</div>
+					</div>
+					<div className={"BannerRight"}>
+						<Img3 className={"Banner2Img"}/>
+					</div>
+				</div>
+				<div className={"BannerWrapperWelcome3"}>
+					<div className={"BannerLeft"}>
+						<Img2 className={"Banner1Img"}/>
+					</div>
+					<div className={"BannerRight"}>
+						<div className={"BannerRight1Header"}>
+							{messages_en.welcomeBanner3}
+						</div>
+						<div className={"BannerRight1SubText"}>
+							{messages_en.welcomeBanner1Sub}
+						</div>
+						<Button 
+							className={"BannerButton"}
+							onClick={() => {console.log("TODO")}}>
+								{messages_en.welcomeBanner1Btn}
+						</Button>
+					</div>
 				</div>
 			</React.Fragment>
 		);
