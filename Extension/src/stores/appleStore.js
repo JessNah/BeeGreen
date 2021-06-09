65f146f6e0c8079432da88dce404013c03efff7b
Jessica Nahulan <JessicaN@ibm.com>
2021-06-09 17:01:17 -0400

    fix applestore logic

--- Extension/src/stores/appleStore.js
+++ Extension/src/stores/appleStore.js
@@ -6,12 +6,12 @@ export function getAppleCart() {
     for(let i = 0; i < _cartItems.length; i++){
         const priceItem = _cartItems[i].getElementsByClassName("rs-iteminfo-price");
         const textItem = _cartItems[i].getElementsByClassName("rs-iteminfo-title");
-        const imageItem = _cartItems[i].getElementsByClassName("as-util-relatedlink")[0].childNodes[0];
+        const imageItem = _cartItems[i].getElementsByClassName("rs-iteminfo-image");
         let _item = {
             id: textItem[0].childNodes[0].innerText?.trim(),
             name: textItem[0].childNodes[0].innerText?.trim(),
-            price: priceItem[0].childNodes[0].childNodes[1].innerText,
-            image: imageItem ? imageItem.currentSrc : ""
+            price: priceItem[0].childNodes[0].innerText.trim(),
+            image: imageItem[0].childNodes[0].currentSrc
         }
         cart.push(_item);
     }
