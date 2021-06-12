90742cffe6fff86d8bf0281ff551daef63242de1
Jessica Nahulan <jess.n169@hotmail.com>
2021-06-12 09:26:26 -0400

    finished bestWorstBuy cards

--- Client/src/DashBoard/DashBoard.js
+++ Client/src/DashBoard/DashBoard.js
@@ -4,6 +4,8 @@ import { Tile } from 'carbon-components-react';
 import './DashBoard.scss';
 import {ReactComponent as ReactSvg} from '../assets/undraw_winners_ao2o.svg';
 import {ReactComponent as ReactSvg2} from '../assets/undraw_empty_street_sfxm.svg';
+import di from '../assets/di.png';
+import micro from '../assets/micro.png'
 import withAnimationEaseIn from "../Utilities/withAnimationEaseIn/withAnimationEaseIn.js";
 import RatingTag from "../RatingTag/RatingTag";
 
@@ -37,6 +39,9 @@ const DashBoard = (props) => {
 						<div className={"TileText"}>
 							Your favorite store to use BeeGreen in is InstaCart.
 						</div>
+						<div className={"TileText"}>
+							You are in the top 5% of shoppers in your region!
+						</div>
 						<div className={"TileTextEmphasize"}>
 							Congratulations, over the last month, you saved 21 kg of CO2
 						</div>
@@ -59,7 +64,7 @@ const DashBoard = (props) => {
 				<div style={{display: "flex", paddingTop: "20px", paddingLeft: "30px", paddingBottom: "0px"}}>
 					<Tile className={"DashboardTileVictory"}>
 						<div className={"DashboardTileHeader"}>
-							How you compare to others in your area
+							How you compare to others in your region
 						</div>
 						<div className={"TileText"}>
 							Your favorite online store is InstaCart.
@@ -69,20 +74,66 @@ const DashBoard = (props) => {
 						</div>
 						<RatingTag score={3} />
 						<div className={"TileText"}>
-							Other InstaCart shoppers in your area have an average cart rank of:
+							Other InstaCart shoppers in your region have an average cart rank of:
 						</div>
 						<RatingTag score={7} />
 						<div className={"TileText"}>
 							Did you know that your average cart is 1.73x better than the national average in the US?
 						</div>
 						<div className={"TileText"}>
-							Your overall online carbon score this month is 2.6x better than others in your area!
+							Your overall online carbon score this month is 2.6x better than others in your region!
 						</div>
 						<div className={"TileTextEmphasize"}>
 							That's incredible! You are setting a standard!
 						</div>
 					</Tile>
 					<Img2 className={"DashboardVictoryImg"} style={{width: 700, maxHeight: "400px", marginLeft: "80px"}}/>
+				</div>
+				<div className={"DashboardTilesWrapper DashboardContent"}>
+					<Tile className={"DashboardTile"}>
+						<div className={"DashboardTileHeader"}>
+							Your lowest scoring buy this year
+						</div>
+						<div style={{display:"flex", justifyContent:"flex-start", paddingTop: "20px"}}>
+							<img src={di} style={{maxHeight: "80px", paddingRight: "30px", paddingLeft: "30px"}}/>
+							<div>
+								<div className={"TileTextEmphasize"} style={{marginTop: "-20px"}}>
+									Tru Earth Hypoallergenic, Eco-friendly and Biodegradable Plastic-Free Laundry Detergent
+								</div>
+								<div className={"TileText"}>
+									Bought from Amazon on 04/24/21
+								</div>
+								<div className={"TileText"}>
+									This item scored:
+								</div>
+								<div style={{marginTop: "10px"}}> 
+									<RatingTag score={0.3} />
+								</div>
+							</div>
+						</div>
+					</Tile>
+					<Tile className={"DashboardTile"}>
+						<div className={"DashboardTileHeader"}>
+							Your most noteworthy buy this year
+						</div>
+						<div style={{display:"flex", justifyContent:"flex-start", paddingTop: "20px"}}>
+							<img src={micro} style={{maxHeight: "90px", paddingRight: "30px", paddingLeft: "20px"}}/>
+							<div>
+								<div className={"TileTextEmphasize"} style={{marginTop: "-10px"}}>
+									Panasonic 2.2 Cu. Ft. Microwave (NNST966W) - White
+								</div>
+								<div className={"TileText"}>
+									Bought from Best Buy on 04/28/21
+								</div>
+								<div className={"TileText"}>
+									This item scored:
+								</div>
+								<div style={{marginTop: "10px"}}> 
+									<RatingTag score={9} />
+								</div>
+							</div>
+						</div>
+					</Tile>
 				</div>
 				<div className={"DashboardTilesWrapper DashboardContent"}>
 					<Tile className={"DashboardTile"}
@@ -125,48 +176,6 @@ const DashBoard = (props) => {
 						</div>
 						<div>
 							
-						</div>
-					</Tile>
-				</div>
-				<div className={"DashboardTilesWrapper DashboardContent"}>
-					<Tile className={"DashboardTile"}>
-						<div className={"DashboardTileHeader"}>
-							Your most noteworthy buy this year
-						</div>
-						<div>
-							Coffee
-						</div>
-						<div>
-							score
-						</div>
-						<div>
-							56k CO emissions
-						</div>
-						<div>
-							04/24/32
-						</div>
-						<div>
-							Instacart
-						</div>
-					</Tile>
-					<Tile className={"DashboardTile"}>
-						<div className={"DashboardTileHeader"}>
-							Your lowest scoring buy this year
-						</div>
-						<div>
-							Coffee
-						</div>
-						<div>
-							score
-						</div>
-						<div>
-							56k CO emissions
-						</div>
-						<div>
-							04/24/32
-						</div>
-						<div>
-							Instacart
 						</div>
 					</Tile>
 				</div>
